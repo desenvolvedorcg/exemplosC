706498dd39f6ef07a302e7c823cf02994343324c
desenvolvedorc <inglescampinagrane@gmail.com>
2017-10-30 14:06:59 -0300

    Ponteiros Em c

---
+++ PonteiroC/ponteiroPart1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+
+int x = 10;
+int *p;
+p = &x;
+
+printf("x = %d\n", x);
+printf("*p = %d\n", *p);
+printf("&x = %p\n", &x);
+printf("p = %p\n",p);
+
+}
+
